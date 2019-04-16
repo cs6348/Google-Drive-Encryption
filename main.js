af6ba182421542ad06d8e338b958c0f9c44e5f48
Ali Sbeiti <at.alisbeiti@gmail.com>
2019-04-16 12:29:26 -0500

    Expanded test file to use renderer page

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-const {app, BrowserWindow} = require('electron');
+'use strict'
+
+import { join } from 'path';
+import { app, BrowserWindow } from 'electron';
 
 let win = null;
 
-app.on('ready', () => {
-    win = new BrowserWindow({
-        width: 1000,
-        height: 850
-    })
+//TUDO: get user prefs from some config file
+const getUserSettings = {
+    width: 1000,
+    height: 650,
+    show: false
+}
 
-    win.loadURL('https://google.com');
+//TUDO: change to render process
+function launchApp(){
+    win = new BrowserWindow(getUserSettings)
+    win.loadFile(join(__dirname,'src','index.html'))
+
+    //Graceful display of renderer window
+    win.once('ready-to-show', () => {
+        win.show();
+    })
+}
+
+//Launch app on ready
+app.on('ready', launchApp)
+
+//Exit process when user closes all GUI
+app.on('window-all-closed', () => {
+    app.quit();
 })
