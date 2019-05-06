575ce5480d8090bfd705b7710f1120c95b814ca8
nathan <15205824+n-elderbroom@users.noreply.github.com>
2019-05-06 15:40:01 -0500

    Now watches for changes in ./Documents. Encrypts the result and stores in ./.uploading. Not sure if files should be uploaded from there, or if they should be uploaded directly.

--- localServer.js
+++ localServer.js
@@ -12,7 +12,7 @@ const generateOAuth = (req, res, next) => {
 const getCode = (req, res, next) => {
     let code = null;
     const index = req.originalUrl.indexOf("=");
-    if(req.originalUrl.indexOf("?code=") != -1) {
+    if (req.originalUrl.indexOf("?code=") != -1) {
         code = req.originalUrl.slice(index + 1, req.originalUrl.length);
         config.setCode(code);
     } else {
@@ -31,12 +31,12 @@ function launchServer() {
     //Custom middleware on GET request to Google sign on
     app.get('/login', generateOAuth);
     app.get('/direct', (req, res, next) => {
-        getCode(req,res,next);
+        getCode(req, res, next);
         config.generateToken(next);
     }, (req, res, next) => {
         res.redirect('/drive');
         next();
-    }); 
+    });
     //app.get('/drive', getCode);
     //Set path aliases for other pages
     //app.use('/login', express.static(path.join(__dirname, 'public', 'pages/login.html')));
@@ -50,9 +50,8 @@ function launchServer() {
 
 var ipc = require('electron').ipcMain;
 
-ipc.on('invokeAction', function(event, data){
-    function sendData(data)
-    {
+ipc.on('invokeAction', function (event, data) {
+    function sendData(data) {
         console.log(data[0][0]);
         console.log('Run Good');
         event.sender.send('actionReply', data);
@@ -61,3 +60,85 @@ ipc.on('invokeAction', function(event, data){
 });
 
 module.exports.launchServer = launchServer;
+
+
+const fs = require('fs');
+const crypto = require('crypto');
+handleDocuments()
+
+async function getSecretKey(callback) { //gets the symmetric key used to store unshared docs
+    fs.stat(".secretkey", function (err, stats) {
+        if (err) {
+            if (err.code == "ENOENT") { // if key doesnt exist, make it
+                let key = crypto.randomBytes(32)
+                fs.writeFile(".secretkey", key, function (err) {
+                    if (err) throw err
+                })
+                return callback(key)
+            } else throw err //throw other errors
+        }
+        fs.readFile(".secretkey", function (err, rawfilecontents) { // if file exists read key
+            if (err) throw err
+            return callback(rawfilecontents)
+        })
+    })
+}
+
+
+function encryptFile(filename) {
+    getSecretKey(function (key) {
+        fs.readFile("./Documents/" + filename, function (err, rawfilecontents) {
+            if (err) {
+                console.log("error opening the file ./Documents/" + filename)
+                console.dir(err)
+                throw err
+            }
+
+            let iv = crypto.randomBytes(16)
+            let cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+            let ciphertext = cipher.update(rawfilecontents, 'binary', 'binary')
+
+            console.log(typeof (ciphertext))
+
+            fs.mkdir("./.uploading", function () {
+                fs.writeFile("./.uploading/" + filename + ".encrypted", ciphertext, { encoding: 'binary' }, function (err) {
+                    if (err) {
+                        console.log("error writing to file ")
+                        console.log("./.uploading/" + filename + ".encrypted")
+                        throw err
+                    }
+                })
+                console.log("IV for file " + filename + " is :")
+                console.dir(iv)
+                //TODO store IV somehow
+                //we can store it on google drive with  "custom file properties", basically metadata https://developers.google.com/drive/api/v3/properties
+                //but not sure how to get it to there?
+
+            })
+        })
+    })
+}
+
+async function handleDocuments(aesKey) {
+    fs.mkdir("./Documents", function () {
+        //this initial one-time read isnt recursive (yet)
+        fs.readdir("./Documents", { withFileTypes: false }, function (err, files) {
+            //get the encryption key and encrypt files
+            for (var file of files) {
+                console.log(file)
+                //open the file:
+                encryptFile(file)
+
+            }
+        })
+        //setup watch on filedirectory
+        fs.watch("./Documents", { recursive: true }, function (eventname, filename) {
+            console.log("CHANGE DETECTED OF TYPE " + eventname + " on file " + filename)
+            //change means file contents changed
+            //rename means creation/deletion/rename
+            if (eventname == "change") {
+                encryptFile(filename)
+            }
+        })
+    })
+}
