0ed46b5a91a46d50c5c32a8953a999f57e13f8b2
nathanle1406 <15205824+n-elderbroom@users.noreply.github.com>
2019-05-11 23:13:50 -0500

    fixed the last commit

--- config/config.js
+++ config/config.js
@@ -73,205 +73,204 @@ class Config {
           fields: 'nextPageToken, files(id, name)'
         },
         (err, res) => {
-      if (err) {
-        console.log('Error getting drive listing' + err);
-        return;
-      }
-      const response = res.data.files;
-      if (response.length) {
-        // console.log('Files:\n-----------------------------------------');
-        response.map((file) => {
-          // console.log(`${file.name} : (${file.id})`);
-          folder.push([file.name, file.id]);
-        });
-        callback(folder);
-      } else {
-        console.log('No Files :(');
-      }
-      this.handleDocuments()
-      for (let obj of folder) {
-        console.log('Downloading')
-        // var dest = fs.createWriteStream('./.downloading/' + obj[0] +
-        // '.dl');
-        if (obj[1] == 'TESTDOWNLOAD')
-        drive.files
-            .get({
-              fileId: obj[1],
-              fields: '*'
-              //   'name, appProperties, createdTime, modifiedTime,
-              //   modifiedByMeTime'
-
-            })
-            .then(function(file) {
-              console.log('Downloaded: ' + file.data.name);
-              console.dir(file.data)
-            })
-            .catch(function(err) {
-              console.log('Error during download', err);
-            })
-      }
-          }
-  });
-}
-
-uploadfile(fileName, ciphertext, iv, callback) {
-  const drive = google.drive({version: 'v3', auth: this.auth});
-  var fileId = '';
-  var fileMetadata;
-  var media = {mimeType: 'application/octet-stream', body: ciphertext};
-  var i;
-  for (i = 0; i < folder.length; i++) {
-    if (fileName == folder[i][0]) {
-      // console.log("Match: "+folder[i][0]);
-      fileId = folder[i][1];
-      i = folder.length;
-    }
-  }
-  if (fileId != '') {
-    fileMetadata = {
-      'name': fileName,
-      'properties': {
-        'IV': iv,
-      },
-      'appProperties': {
-        'IV': iv,
-      }
-    };
-    console.log(fileId);
-    drive.files.update(
-        {fileId: fileId, resource: fileMetadata, media: media, fields: 'id'})
-  } else {
-    fileMetadata = {
-      'name': fileName,
-      'parents': ['appDataFolder'],
-      'properties': {
-        'IV': iv,
-      },
-      'appProperties': {
-        'IV': iv,
-      }
-    };
-    drive.files.create(
-        {resource: fileMetadata, media: media, fields: 'id'},
-        function(err, file) {
           if (err) {
-            // Handle error
-            console.error(err);
+            console.log('Error getting drive listing' + err);
+            return;
+          }
+          const response = res.data.files;
+          if (response.length) {
+            // console.log('Files:\n-----------------------------------------');
+            response.map((file) => {
+              // console.log(`${file.name} : (${file.id})`);
+              folder.push([file.name, file.id]);
+            });
+            callback(folder);
           } else {
-            console.log(
-                'Uploaded file \"' + fileName + '\", File Id: ', file.id);
+            console.log('No Files :(');
+          }
+          this.handleDocuments()
+          for (let obj of folder) {
+            // var dest = fs.createWriteStream('./.downloading/' + obj[0] +
+            // '.dl');
+            if (obj[0] == 'TESTDOWNLOAD') {
+              console.log('trying to download TESTDOWNLOAD')
+              drive.files
+                  .get({
+                    fileId: obj[1],
+                    fields: '*'
+                    //   'name, appProperties, createdTime, modifiedTime,
+                    //   modifiedByMeTime'
+
+                  })
+                  .then(function(file) {
+                    console.log('Downloaded: ' + file.data.name);
+                    console.dir(file.data)
+                  })
+                  .catch(function(err) {
+                    console.log('Error during download', err);
+                  })
+            }
           }
         });
   }
-  // Still need to attack the IV to the file
-  callback()
-}
 
-getSecretKey(callback) {  // gets the symmetric key used to store unshared
-                          // docs
-  fs.stat('.secretkey', function(err, stats) {
-    if (err) {
-      if (err.code == 'ENOENT') {  // if key doesnt exist, make it
-        let key = crypto.randomBytes(32)
-        fs.writeFile('.secretkey', key, function(err) {
-          if (err) throw err
-        })
-        return callback(key)
-      } else
-        throw err  // throw other errors
+  uploadfile(fileName, ciphertext, iv, callback) {
+    const drive = google.drive({version: 'v3', auth: this.auth});
+    var fileId = '';
+    var fileMetadata;
+    var media = {mimeType: 'application/octet-stream', body: ciphertext};
+    var i;
+    for (i = 0; i < folder.length; i++) {
+      if (fileName == folder[i][0]) {
+        // console.log("Match: "+folder[i][0]);
+        fileId = folder[i][1];
+        i = folder.length;
+      }
     }
-    fs.readFile(
-        '.secretkey',
-        function(err, rawfilecontents) {  // if file exists read key
-          if (err) throw err
-            return callback(rawfilecontents)
-        })
-  })
-}
+    if (fileId != '') {
+      fileMetadata = {
+        'name': fileName,
+        'properties': {
+          'IV': iv,
+        },
+        'appProperties': {
+          'IV': iv,
+        }
+      };
+      console.log(fileId);
+      drive.files.update(
+          {fileId: fileId, resource: fileMetadata, media: media, fields: 'id'})
+    } else {
+      fileMetadata = {
+        'name': fileName,
+        'parents': ['appDataFolder'],
+        'properties': {
+          'IV': iv,
+        },
+        'appProperties': {
+          'IV': iv,
+        }
+      };
+      drive.files.create(
+          {resource: fileMetadata, media: media, fields: 'id'},
+          function(err, file) {
+            if (err) {
+              // Handle error
+              console.error(err);
+            } else {
+              console.log(
+                  'Uploaded file \"' + fileName + '\", File Id: ', file.id);
+            }
+          });
+    }
+    // Still need to attack the IV to the file
+    callback()
+  }
 
-shareFile(filename, user) {
-  // download filename's key file of username user
-
-  // download key sharing file
-
-  // decrypt the symmetric key with your private key pair
-
-  // fetch user's public key from the public key folder
-
-  // encrypt symmetric key with user's public key
-
-  // append to the filename's key file
-
-  // overwrite key file with new key file
-}
-encryptFile(filename) {
-  let self = this;  // so we can get `this` inside anonymous functions
-  this.getSecretKey(function(key) {
-    fs.readFile('./Documents/' + filename, function(err, rawfilecontents) {
+  getSecretKey(callback) {  // gets the symmetric key used to store unshared
+                            // docs
+    fs.stat('.secretkey', function(err, stats) {
       if (err) {
-        console.log('error opening the file ./Documents/' + filename)
-        console.dir(err)
-        throw err
+        if (err.code == 'ENOENT') {  // if key doesnt exist, make it
+          let key = crypto.randomBytes(32)
+          fs.writeFile('.secretkey', key, function(err) {
+            if (err) throw err
+          })
+          return callback(key)
+        } else
+          throw err  // throw other errors
       }
+      fs.readFile(
+          '.secretkey',
+          function(err, rawfilecontents) {  // if file exists read key
+            if (err) throw err
+              return callback(rawfilecontents)
+          })
+    })
+  }
 
-      let iv = crypto.randomBytes(16)
-      let cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
-      let ciphertext = cipher.update(rawfilecontents, 'binary', 'binary')
+  shareFile(filename, user) {
+    // download filename's key file of username user
 
-      console.log('cipher is of type' + typeof (ciphertext))
+    // download key sharing file
+
+    // decrypt the symmetric key with your private key pair
+
+    // fetch user's public key from the public key folder
+
+    // encrypt symmetric key with user's public key
+
+    // append to the filename's key file
+
+    // overwrite key file with new key file
+  }
+  encryptFile(filename) {
+    let self = this;  // so we can get `this` inside anonymous functions
+    this.getSecretKey(function(key) {
+      fs.readFile('./Documents/' + filename, function(err, rawfilecontents) {
+        if (err) {
+          console.log('error opening the file ./Documents/' + filename)
+          console.dir(err)
+          throw err
+        }
+
+        let iv = crypto.randomBytes(16)
+        let cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+        let ciphertext = cipher.update(rawfilecontents, 'binary', 'binary')
 
 
-      self.uploadfile(filename, ciphertext, iv, function() {
-        console.log('FINISHED AN UPLOAD')
+
+        self.uploadfile(filename, ciphertext, iv, function() {
+          console.log('FINISHED AN UPLOAD')
+        })
+
+        // fs.mkdir("./.uploading", function () {
+        //     fs.writeFile("./.uploading/" + filename + ".encrypted",
+        //     ciphertext, { encoding: 'binary' }, function (err) {
+        //         if (err) {
+        //             console.log("error writing to file ")
+        //             console.log("./.uploading/" + filename + ".encrypted")
+        //             throw err
+        //         }
+        //     })
+        //     console.log("IV for file " + filename + " is :")
+        //     console.dir(iv)
+        //     //TODO store IV somehow
+        //     //we can store it on google drive with  "custom file properties",
+        //     basically metadata
+        //     https://developers.google.com/drive/api/v3/properties
+        //     //but not sure how to get it to there?
+
+        // })
       })
-
-      // fs.mkdir("./.uploading", function () {
-      //     fs.writeFile("./.uploading/" + filename + ".encrypted",
-      //     ciphertext, { encoding: 'binary' }, function (err) {
-      //         if (err) {
-      //             console.log("error writing to file ")
-      //             console.log("./.uploading/" + filename + ".encrypted")
-      //             throw err
-      //         }
-      //     })
-      //     console.log("IV for file " + filename + " is :")
-      //     console.dir(iv)
-      //     //TODO store IV somehow
-      //     //we can store it on google drive with  "custom file properties",
-      //     basically metadata
-      //     https://developers.google.com/drive/api/v3/properties
-      //     //but not sure how to get it to there?
-
-      // })
     })
-  })
-}
-handleDocuments() {
-  let self = this;  // so we can get `this` inside anonymous functions
-  fs.mkdir('./Documents', function() {
-    // this initial one-time read isnt recursive (yet)
-    fs.readdir('./Documents', {withFileTypes: false}, function(err, files) {
-      // get the encryption key and encrypt files
-      for (var file of files) {
-        console.log(file)
-        // open the file:
-        self.encryptFile(file)
-      }
+  }
+  handleDocuments() {
+    let self = this;  // so we can get `this` inside anonymous functions
+    fs.mkdir('./Documents', function() {
+      // this initial one-time read isnt recursive (yet)
+      fs.readdir('./Documents', {withFileTypes: false}, function(err, files) {
+        // get the encryption key and encrypt files
+        for (var file of files) {
+          // console.log(file)
+          // open the file:
+          self.encryptFile(file)
+        }
+      })
+      // setup watch on filedirectory
+      fs.watch('./Documents', {recursive: true}, function(eventname, filename) {
+        console.log(
+            'CHANGE DETECTED OF TYPE ' + eventname + ' on file ' + filename)
+        // change means file contents changed
+        // rename means creation/deletion/rename
+        if (eventname == 'change') {
+          self.encryptFile(
+              filename,
+          )
+        }
+      })
     })
-    // setup watch on filedirectory
-    fs.watch('./Documents', {recursive: true}, function(eventname, filename) {
-      console.log(
-          'CHANGE DETECTED OF TYPE ' + eventname + ' on file ' + filename)
-      // change means file contents changed
-      // rename means creation/deletion/rename
-      if (eventname == 'change') {
-        self.encryptFile(
-            filename,
-        )
-      }
-    })
-  })
-}
+  }
 }
 
 
