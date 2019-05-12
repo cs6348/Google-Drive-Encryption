290a379e3b7d69b1d67276ee9ac8cb7a00d76ece
Ali Sbeiti <at.alisbeiti@gmail.com>
2019-05-11 19:28:49 -0500

    Redesigned UI

--- public/js/display.js
+++ public/js/display.js
@@ -5,21 +5,36 @@ function printFiles(folder)
     var i
     for(i = 0; i<folder.length; i++)
     {
-        file.push(document.createElement("div"));
-        file[i].setAttribute('class', 'drive-item');
+        // file.push(document.createElement("tr"));
+        // file[i].setAttribute('class', 'drive-item');
+        // file[i].id = i;
+        // file[i].innerHTML = `
+        // <div class="item-inner">
+        // <img class="thumb" src=${folder[i][2]} onerror="this.onerror=null;this.src='../icons/fileicon.png';"> 
+        // </br>
+        // <div class="info-box">
+        // <img src=${folder[i][3]}> 
+        // </br> ${folder[i][0]}
+        // </div>
+        // </div>`;
+
+        // var box = document.getElementById("folder");
+        // box.appendChild(file[i]);
+
+        // file[i].addEventListener ("click", function() {
+        //     alert("Download ID: "+folder[this.id][1]);
+        // });
+
+        file.push(document.createElement('tr'));
+        file[i].setAttribute('class', 'file-item');
         file[i].id = i;
         file[i].innerHTML = `
-        <div class="item-inner">
-        <img class="thumb" src=${folder[i][2]} onerror="this.onerror=null;this.src='../icons/fileicon.png';"> 
-        </br>
-        <div class="info-box">
-        <img src=${folder[i][3]}> 
-        </br> ${folder[i][0]}
-        </div>
-        </div>`;
+        <th scope="row"> ${i} </th>
+        <td> <img src=${folder[i][3]}> </td>
+        <td> ${folder[i][0]} </td>
+        `;
 
-        var box = document.getElementById("folder");
-        document.body.appendChild(file[i]);
+        document.getElementById('fileCollection').appendChild(file[i]);
 
         file[i].addEventListener ("click", function() {
             alert("Download ID: "+folder[this.id][1]);
